eaf9d6fa45616096bc61907b33a4f74673d0f941
behrouzsh <behrouzsharifi7@gmail.com>
2024-11-10 01:07:28 +0330

    custom validators

--- A/accounts/serializers.py
+++ A/accounts/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
 
 
+def clean_email(value):
+    if 'admin' in value:
+        raise serializers.ValidationError('Email cannot contain the word admin')
+
+
+
+
+
 class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required = True)
-    email = serializers.EmailField(required = True)
+    email = serializers.EmailField(required = True, validators = [clean_email])
     password = serializers.CharField(required = True, write_only = True)
+    password2 = serializers.CharField(required = True, write_only = True)
 
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError('Username cannot be admin')
+        return value
+    
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('Passwords do not match')
+        return data
+    
