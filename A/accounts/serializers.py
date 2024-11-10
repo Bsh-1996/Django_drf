bcc45bc41f8403f9524cbd7a3498ffe0f72998cb
behrouzsh <behrouzsharifi7@gmail.com>
2024-11-10 13:40:48 +0330

    overriding create

--- A/accounts/serializers.py
+++ A/accounts/serializers.py
@@ -1,19 +1,26 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 
 def clean_email(value):
     if 'admin' in value:
         raise serializers.ValidationError('Email cannot contain the word admin')
+    
 
 
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only = True, required = True)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'validators': (clean_email,)}
+        }
 
-
-
-class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required = True)
-    email = serializers.EmailField(required = True, validators = [clean_email])
-    password = serializers.CharField(required = True, write_only = True)
-    password2 = serializers.CharField(required = True, write_only = True)
+    def create(self, validated_data):
+        del validated_data['password2']
+        return User.objects.create_user(**validated_data)
+    
 
 
     def validate_username(self, value):
@@ -21,8 +28,11 @@ class UserRegisterSerializer(serializers.Serializer):
             raise serializers.ValidationError('Username cannot be admin')
         return value
     
+
     def validate(self, data):
         if data['password'] != data['password2']:
             raise serializers.ValidationError('Passwords do not match')
         return data
     
+
+
