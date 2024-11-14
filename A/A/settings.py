9bb198d91fadc38089ae94d29efc809deb4b9068
behrouzsh <behrouzsharifi7@gmail.com>
2024-11-14 14:01:46 +0330

    renderer/parserer

--- A/A/settings.py
+++ A/A/settings.py
@@ -139,6 +139,9 @@ REST_FRAMEWORK = {
           'questions': '5/minute',
     },
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 
 
