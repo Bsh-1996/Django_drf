adebb58051d64dedcaaf23c37eaafe677662dd11
behrouzsh <behrouzsharifi7@gmail.com>
2024-11-14 17:01:47 +0330

    metadata

--- A/A/settings.py
+++ A/A/settings.py
@@ -141,7 +141,8 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 2,
+    'DEFAULT_METADATA_CLASS': 'permissions.CustomMetadata',
 }
 
 
